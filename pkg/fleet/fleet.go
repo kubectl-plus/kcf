c5bee36a97eda445f67166fb28d60286c8e3e41b
Michael Hausenblas <michael.hausenblas@gmail.com>
2019-11-27 14:59:32 +0000

    changes top-level command to tabular output

--- pkg/fleet/fleet.go
+++ pkg/fleet/fleet.go
@@ -2,9 +2,10 @@ package fleet
 
 import (
 	"fmt"
-
 	"github.com/pkg/errors"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"os"
+	"text/tabwriter"
 )
 
 // RunFleetCommand runs the top-level fleet command
@@ -13,14 +14,16 @@ func RunFleetCommand(configFlags *genericclioptions.ConfigFlags) error {
 	if err != nil {
 		return errors.Wrap(err, "failed to read kubeconfig")
 	}
-	fmt.Printf("NAME\tCLUSTER\tSERVER\n")
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.TabIndent)
+	fmt.Fprintln(w, "NAME\tCLUSTER\tSERVER")
 	for name, context := range config.Contexts {
 		cluster := config.Clusters[context.Cluster]
 		server := "unknown"
 		if cluster != nil {
 			server = cluster.Server
 		}
-		fmt.Printf("%v\t%v\t%v\n", name, context.Cluster, server)
+		fmt.Fprintln(w, fmt.Sprintf("%v\t%v\t%v", name, context.Cluster, server))
 	}
+	w.Flush()
 	return nil
 }
