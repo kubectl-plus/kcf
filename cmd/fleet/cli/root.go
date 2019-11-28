3b48515bc3a6be8f8e7cd3e5fb9f3832f2521fd0
Michael Hausenblas <michael.hausenblas@gmail.com>
2019-11-28 10:09:07 +0000

    removes logger from root command

--- cmd/fleet/cli/root.go
+++ cmd/fleet/cli/root.go
@@ -6,7 +6,6 @@ import (
 	"strings"
 
 	"github.com/mhausenblas/kcf/pkg/fleet"
-	"github.com/mhausenblas/kcf/pkg/logger"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -14,9 +13,11 @@ import (
 )
 
 var (
+	// KubernetesConfigFlags makes the config flags available globally
 	KubernetesConfigFlags *genericclioptions.ConfigFlags
 )
 
+// RootCmd runs the fleet root command
 func RootCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:           "fleet",
@@ -28,25 +29,20 @@ func RootCmd() *cobra.Command {
 			viper.BindPFlags(cmd.Flags())
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			log := logger.NewLogger()
-			log.Info("")
 			if err := fleet.RunFleetCommand(KubernetesConfigFlags); err != nil {
 				return errors.Cause(err)
 			}
-			log.Info("")
 			return nil
 		},
 	}
-
 	cobra.OnInitialize(initConfig)
-
 	KubernetesConfigFlags = genericclioptions.NewConfigFlags(false)
 	KubernetesConfigFlags.AddFlags(cmd.Flags())
-
 	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 	return cmd
 }
 
+// InitAndExecute sets up and executes the root command
 func InitAndExecute() {
 	if err := RootCmd().Execute(); err != nil {
 		fmt.Println(err)
