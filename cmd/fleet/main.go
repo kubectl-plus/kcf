8af107d40596b4d2cf91b46d71442c3dd96454ef
naemono <mmontg1@gmail.com>
2020-02-14 16:09:08 -0600

    Just have a single import for both gke, and oidc

--- cmd/fleet/main.go
+++ cmd/fleet/main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"github.com/mhausenblas/kcf/cmd/fleet/cli"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"  // required for GKE
-	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc" // required for oidc authentication
+	_ "k8s.io/client-go/plugin/pkg/client/auth" // required for GKE/OIDC/Dex style auth
 )
 
 func main() {
